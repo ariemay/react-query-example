8a3efc8d83145825d34bda1244809798ecaa849d
Arie May Wibowo <ariemayw@gmail.com>
2021-02-08 04:16:02 +0700

    push learning codes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import Home from './components/Home'
+import Post from './components/Post'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path = '/post/:id' render = {routerProps => <Post id={routerProps.match.params.id}/>} />
+      </Switch>
+    </Router>
+  )
 }
 
-export default App;
+export default App
